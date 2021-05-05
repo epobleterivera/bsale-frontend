23a3d9318aaaccec56d0b8db494e24ac65ae8be6
epoblete <epobleterivera@gmail.com>
2021-05-05 07:12:42 -0400

    fix url Heroku en app.js frontend

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const noImage = new Image();
 noImage.src = './image/noImage.jpg';
 
 // Ruta para  connexión a la API del backend
-const url = "http://localhost:3001/api/product";
-const urlCategory = "http://localhost:3001/api/category";
+const url = "https://bsale-tienda-app.herokuapp.com/api/product";
+const urlCategory = "https://bsale-tienda-app.herokuapp.com/api/category";
 
 //evento que se dispara cuando se aprieta cualquier tecla dentro del input buscador
 //captura lo que va escribiendo el usuario y lo va guardando en una variable
